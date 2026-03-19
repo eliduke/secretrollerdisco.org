e5b3ca67e41faabd4454f5b8eea870eb2fa4bf6b
Eli Duke <elimduke@gmail.com>
2026-03-19 14:41:17 -0700

    another round of refactoring for new ruby script / github action

--- rb/check_for_none.rb
+++ rb/check_for_none.rb
@@ -4,9 +4,11 @@ last_disco = Dir['_posts/*'].last
 
 return unless last_disco.include?('none')
 
-_, file    = last_disco.split('/')
-curr_date  = file.split('-')[0..2].join('-')
-next_date  = Date.parse(curr_date) + 7
-next_disco = "_posts/#{next_date}-none.md"
+file_name  = last_disco.split('/').last
+last_date  = Date.parse(file_name.split('-')[0..2].join('-'))
+
+return if last_date == Date.today
+
+next_disco = "_posts/#{Date.today}-none.md"
 
 File.rename(last_disco, next_disco)
