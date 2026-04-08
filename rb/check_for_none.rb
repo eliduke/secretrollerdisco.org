ba96f04b74c11eb29300b34fb980aaa0520c56d0
Eli Duke <elimduke@gmail.com>
2026-04-07 18:37:02 -0700

    just removes some unnecessary spaces

--- rb/check_for_none.rb
+++ rb/check_for_none.rb
@@ -4,8 +4,8 @@ last_disco = Dir['_posts/*'].last
 
 return unless last_disco.include?('none')
 
-file_name  = last_disco.split('/').last
-last_date  = Date.parse(file_name.split('-')[0..2].join('-'))
+file_name = last_disco.split('/').last
+last_date = Date.parse(file_name.split('-')[0..2].join('-'))
 
 return if last_date == Date.today
 
